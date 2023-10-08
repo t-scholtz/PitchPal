7a2a2a1edfa351c69ac752ac7e23549eec2f94e7
TScholtz <tim.scholtz@outlook.com>
2023-10-08 18:08:24 -0400

    init

---
+++ examples/simpleButtonSetup/ledExample.ino
@@ -0,0 +1,24 @@
+//Pin 3: input fom btn
+//Pin 2: output LED
+int ButtonValue = 0;
+
+int BUTTON = 3;
+int LED = 2;
+
+void setup() {
+  pinMode(BUTTON, INPUT);
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  ButtonValue = digitalRead(BUTTON);
+
+  if (ButtonValue != 0){
+      digitalWrite(LED, HIGH);
+  }
+  else{
+      digitalWrite(LED, LOW);
+  }
+
+}
