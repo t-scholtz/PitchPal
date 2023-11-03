c44b325416d94110c2a25b156fda2e8efe3c76b0
TScholtz <tim.scholtz@outlook.com>
2023-11-03 16:00:30 -0400

    Re-wrote note array

--- src/main/main.ino
+++ src/main/main.ino
@@ -11,22 +11,22 @@
 #include "output.h" 
 #include "pitches.h"
 
+int noteArray[][] = {
+/*A*/     {NOTE_A1, NOTE_A2, NOTE_A3, NOTE_A4, NOTE_A5, NOTE_A6, NOTE_A7},
+/*A#Bb*/  {NOTE_AS1, NOTE_AS2, NOTE_AS3, NOTE_AS4, NOTE_AS5, NOTE_AS6, NOTE_AS7},
+/*B*/     {NOTE_B1, NOTE_B2, NOTE_B3, NOTE_B4, NOTE_B5, NOTE_B6, NOTE_B7},
+/*C*/     {NOTE_C1, NOTE_C2, NOTE_C3, NOTE_C4, NOTE_C5, NOTE_C6, NOTE_C7},
+/*C#Db*/  {NOTE_CS1, NOTE_CS2, NOTE_CS3, NOTE_CS4, NOTE_CS5, NOTE_CS6, NOTE_CS7},
+/*D*/     {NOTE_D1, NOTE_D2, NOTE_D3, NOTE_D4, NOTE_D5, NOTE_D6, NOTE_D7},
+/*D#Eb*/  {NOTE_DS1, NOTE_DS2, NOTE_DS3, NOTE_DS4, NOTE_DS5, NOTE_DS6, NOTE_DS7},
+/*E*/     {NOTE_E1, NOTE_E2, NOTE_E3, NOTE_E4, NOTE_E5, NOTE_E6, NOTE_E7},
+/*F*/     {NOTE_F1, NOTE_F2, NOTE_F3, NOTE_F4, NOTE_F5, NOTE_F6, NOTE_F7},
+/*F#/Gb*/ {NOTE_FS1, NOTE_FS2, NOTE_FS3, NOTE_FS4, NOTE_FS5, NOTE_FS6, NOTE_FS7},
+/*G*/     {NOTE_G1, NOTE_G2, NOTE_G3, NOTE_G4, NOTE_G5, NOTE_G6, NOTE_G7},
+/*G#/Ab*/ {NOTE_GS1, NOTE_GS2, NOTE_GS3, NOTE_GS4, NOTE_GS5, NOTE_GS6, NOTE_GS7},
+}
 
-//note lists to choose from
-//A-1, normal on odd, sharp is on even
-#define noSharp  7
-#define Sharp  14
-int AOption[Sharp] = {NOTE_A1, NOTE_AS1, NOTE_A2, NOTE_AS2, NOTE_A3, NOTE_AS3, NOTE_A4, NOTE_AS4, NOTE_A5, NOTE_AS5, NOTE_A6, NOTE_AS6, NOTE_A7, NOTE_AS7};
-int BOption[noSharp] = {NOTE_B1, NOTE_B2, NOTE_B3, NOTE_B4, NOTE_B5, NOTE_B6, NOTE_B7};
-int COption[Sharp] = {NOTE_C1, NOTE_CS1, NOTE_C2, NOTE_CS2, NOTE_C3, NOTE_CS3, NOTE_C4, NOTE_CS4, NOTE_C5, NOTE_CS5, NOTE_C6, NOTE_CS6, NOTE_C7, NOTE_CS7};
-int DOption[Sharp] = {NOTE_D1, NOTE_DS1, NOTE_D2, NOTE_DS2, NOTE_D3, NOTE_DS3, NOTE_D4, NOTE_DS4, NOTE_D5, NOTE_DS5, NOTE_D6, NOTE_DS6, NOTE_D7, NOTE_DS7};
-int EOption[noSharp] = {NOTE_E1, NOTE_E2, NOTE_E3, NOTE_E4, NOTE_E5, NOTE_E6, NOTE_E7};
-int FOption[Sharp] = {NOTE_F1, NOTE_FS1, NOTE_F2, NOTE_FS2, NOTE_F3, NOTE_FS3, NOTE_F4, NOTE_FS4, NOTE_F5, NOTE_FS5, NOTE_F6, NOTE_FS6, NOTE_F7, NOTE_FS7};
-int GOption[Sharp] = {NOTE_G1, NOTE_GS1, NOTE_G2, NOTE_GS2, NOTE_G3, NOTE_GS3, NOTE_G4, NOTE_GS4, NOTE_G5, NOTE_GS5, NOTE_G6, NOTE_GS6, NOTE_G7, NOTE_GS7};
 
-//This array is temporary but would make the others easy to find
-int SNotes[5] = {*AOption, *COption, *DOption, *FOption, *GOption};//check if we need to add size to the outside when inizilizing or inside the array. If we can do it inside we should be able to do one list
-int NSNotes[2] = {*BOption, *EOption};
 
 void setup() {
   Serial.begin(SERIAL_RATE);
